1992f75acd300a5547785be415ca530f07f1beaf
*MlKw0O2qj8 <hungnt2@fabbi.io>
2022-08-03 13:56:16 +0700

    feat:feat-fake-auth

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import LoginAPIView, AuthenticationAPIView, CheckHealth
+
+
+app_name = 'api'
+
+urlpatterns = [
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('auth/verify/', AuthenticationAPIView.as_view(), name='auth'),
+    path('health/', CheckHealth.as_view(), name='check_health'),
+]
